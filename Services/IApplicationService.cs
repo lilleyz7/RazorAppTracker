081bb805037efa1d15edd38d09f25615408ca36d
lilleyz7 <lilley5>
2025-05-15 23:48:10 -0700

    Async methods

--- Services/IApplicationService.cs
+++ Services/IApplicationService.cs
@@ -1,13 +1,16 @@
-﻿namespace AppTrackV2.Services
+﻿using AppTrackV2.Models;
+using AppTrackV2.DTO;
+
+namespace AppTrackV2.Services
 {
     public interface IApplicationService
     {
-        ICollection<Application> GetApplications(string userId);
-        ICollection<Application> GetByTitleAsync(string title, string userId);
-        Task<Application> GetByIdAsync(Guid id, string userId);
+        IEnumerable<Application> GetApplicationsByUserAsync(string userId);
+        IEnumerable<Application> GetApplicationByTitleAsync(string title, string userId);
+        Task<Application> GetApplicationByIdAsync(Guid id, string userId);
 
-        Task AddApplication(ApplicationDto application, string userId);
-        Task UpdateApplication(ApplicationDto application, string userId, Guid appId);
-        Task DeleteApplication(Guid id, string userId);
+        Task AddApplicationAsync(ApplicationDto application, string userId);
+        Task UpdateApplicationAsync(ApplicationDto application, string userId, Guid appId);
+        Task DeleteApplicationAsync(Guid id, string userId);
     }
 }
